54a782dffa0d3d080eb97ec4018d8fa2c1cbb6a7
MatheusBrazolin <matheusbrazolin013@gmail.com>
2022-04-06 14:42:27 -0300

    Csharp

---
+++ Contagem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Contagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"posicão {i} valor que deseja somar a {valor}: ");
+            }
+
+            Console.WriteLine($"Somatoria: {valor}");
+        }
+    }
+}
+
+
+
