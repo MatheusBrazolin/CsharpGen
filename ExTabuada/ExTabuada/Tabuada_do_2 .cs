53d57c04fa323afc319a51122b2b8eb75d8d18b1
MatheusBrazolin <matheusbrazolin013@gmail.com>
2022-04-07 16:18:25 -0300

    Csharp

---
+++ ExTabuada/ExTabuada/Tabuada_do_2 .cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExTabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Tabuada");
+            Console.WriteLine("Insira o numero que voce quer multiplicar");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            for(int x = 1; x <= 10; x++)
+            {
+                int resultado = numero * x;
+                Console.WriteLine($"{numero} x {x} = {resultado}");
+
+            }
+        }
+    }
+}
---
+++ Tabuada/Tabuada/Tabuada/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada");
+
+            for (int b = 1; b <= 10; b++)
+            {
+                for (int p = 1; p <= 10; p++)
+                {
+                    Console.WriteLine($"{b} x { p} = {b*p}" );
+                    }
+            }
+        }
+    }
+}
